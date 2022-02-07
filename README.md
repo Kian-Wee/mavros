12a204f055fa1d345d1e70f640fb9aba9ffb8e1b
Kian Wee <kianweetan2002@gmail.com>
2022-02-07 15:22:20 +0800

    Updated setup script with udev rules

--- README.md
+++ README.md
@@ -13,14 +13,21 @@ Initalise this fork from master branch
 	4. git checkout borealis
 	
 	5. git pull origin borealis
-	
+
 If any prior changes were made, drop the changes first with, ```git stash```, ```git stash drop stash@{0}```. Enter name and email if prompted.
 
+
+
+
+
+**Vehicle and position setup** 
 Run the setup file to initalise aliases
 ``` . ~/catkin_ws/src/mavros/borealis_setup/setup.bash ```
+Enable vision fusion on EKF_2_AID_MASK
+Do remember to change the MAV_SYS_ID on different vehicles
 
 
-MAVROS
+SMAVROS
 ======
 [![GitHub release (latest by date)](https://img.shields.io/github/v/release/mavlink/mavros)](https://github.com/mavlink/mavros/releases)  [![Gitter](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/mavlink/mavros?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)  [![CI](https://github.com/mavlink/mavros/actions/workflows/main.yml/badge.svg)](https://github.com/mavlink/mavros/actions/workflows/main.yml)
 
--- borealis_setup/setup.bash
+++ borealis_setup/setup.bash
@@ -5,6 +5,8 @@
 cp ~/catkin_ws/src/mavros/borealis_setup/parallel_comments.bash ~
 
 cat >> ~/.bashrc << EOF
+source ~/catkin_ws/devel/setup.bash
+
 alias launch_d435i='roslaunch realsense2_camera rs_camera.launch camera:=uav0/d435i serial_no:=134222075005 color_width:=848 color_height:=480 color_fps:=30 depth_width:=848 depth_height:=480 depth_fps:=30 align_depth:=true'
 alias launch_t265='roslaunch realsense2_camera rs_t265.launch serial_no:=948422110423 camera:=uav0/t265'
 
@@ -13,3 +15,18 @@ alias drone_data_feeder='rosrun drone_data_feeder_node drone_data_feeder.py'
 
 alias launch_borealis='. ~/parallel_comments.bash "roslaunch mavros px4_swarm_extra.launch" "roslaunch realsense2_camera rs_t265.launch camera:=uav0/t265 serial_no:=948422110423" "rosrun mavros pos_265" "roslaunch realsense2_camera rs_camera.launch camera:=uav0/d435i serial_no:=134222075005 color_width:=848 color_height:=480 color_fps:=30 depth_width:=848 depth_height:=480 depth_fps:=30 align_depth:=true"'
 EOF
+
+# Add in udev rules
+
+sudo touch "/etc/udev/rules.d/99-pixhawk.rules"
+sudo su -
+cat <<EOF > /etc/udev/rules.d/99-pixhawk.rules
+SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", SYMLINK+="ttyPixhawk"
+EOF
+
+usermod -a -G tty ros-user
+usermod -a -G dialout ros-user
+
+exit
+
+echo "Please reboot if permissions were not set before"
